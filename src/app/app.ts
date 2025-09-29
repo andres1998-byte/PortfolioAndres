4eb569fb516d0a79c951f909b6c8f2ed82636fdf
andres1998-byte <amvillarreal98@gmail.com>
2025-09-29 00:58:43 -0600

    Main layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { Hero } from './sections/hero/hero';
+import { About } from './sections/about/about';
+import { Experience } from './sections/experience/experience';
+import { Contact } from './sections/contact/contact';
+import { Navbar } from './sections/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [Hero, About, Experience, Contact, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
